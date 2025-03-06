565aec1a4f02ab3630f90cefc445cbff41159cff
pakosam <pako.s@yahoo.com>
2025-03-07 00:06:56 +0100

    Cleaning useless files

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div>
       
-      </header>
     </div>
   );
 }
