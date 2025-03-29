f41e2cd890cc06eb5c6f69fee2a18a993dfc84cc
pakosam <pako.s@yahoo.com>
2025-03-29 14:07:25 +0100

    Navigation.tsx modified

--- src/components/Sidebar/Navigation.tsx
+++ src/components/Sidebar/Navigation.tsx
@@ -3,34 +3,44 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 import { sidebarItems } from "./sidebarItems/sidebarItems";
 import { useLocation } from "react-router-dom";
+import { Location } from "react-router-dom";
+
+
 
 export const Navigation = () => {
   const location = useLocation();
 
+  const Entity = ({ location }: { location: Location }) => {
+    return (
+      <>
+        {sidebarItems.map((item, index) => {
+          const { url, name, Icon } = item;
+          const isItemActive = () => {
+            return location.pathname === url ? "active" : "";
+          };
+          return (
+            <div className={`single-entity ${isItemActive()}`} key={index}>
+              <div className="navbar-icon">
+                <Icon />
+              </div>
+              <div className="navbar-text">
+                <span>
+                  <Link to={url}>{name}</Link>
+                </span>
+              </div>
+            </div>
+          );
+        })}
+        ;
+      </>
+    );
+  };
+  
   return (
     <>
       <div id="Navigation">
         <div className="entities">
-          {sidebarItems.map((item, index) => {
-            const { url, name, Icon } = item;
-            return (
-              <div
-                className={`single-entity ${
-                  location.pathname === url ? "active" : ""
-                }`}
-                key={index}
-              >
-                <div className="navbar-icon">
-                  <Icon />
-                </div>
-                <div className="navbar-text">
-                  <span>
-                    <Link to={url}>{name}</Link>
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+          <Entity location={location}/>
         </div>
       </div>
       <div className="logout-navbar">
