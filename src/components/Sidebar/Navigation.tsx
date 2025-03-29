44c8700f61360aa3ae09c642ed42e142306e95e6
pakosam <pako.s@yahoo.com>
2025-03-29 18:27:11 +0100

    Sidebar fully optimized

--- src/components/Sidebar/Navigation.tsx
+++ src/components/Sidebar/Navigation.tsx
@@ -1,11 +1,16 @@
 import { LogoutIcon } from "../../assets/icons/LogoutIcon";
-import { Link } from "react-router-dom";
+import { Link, Location } from "react-router-dom";
 import "./Navigation.css";
 import { sidebarItems } from "./sidebarItems/sidebarItems";
 import { useLocation } from "react-router-dom";
+import { JSX } from "react";
 
 export const Navigation = () => {
   const location = useLocation();
+
+  const isItemActive = (url: string) => {
+    return location.pathname === url ? "active" : "";
+  };
 
   return (
     <>
@@ -14,21 +19,13 @@ export const Navigation = () => {
           {sidebarItems.map((item, index) => {
             const { url, name, Icon } = item;
             return (
-              <div
-                className={`single-entity ${
-                  location.pathname === url ? "active" : ""
-                }`}
+              <SidebarEntity
                 key={index}
-              >
-                <div className="navbar-icon">
-                  <Icon />
-                </div>
-                <div className="navbar-text">
-                  <span>
-                    <Link to={url}>{name}</Link>
-                  </span>
-                </div>
-              </div>
+                url={url}
+                name={name}
+                Icon={Icon}
+                isItemActive={isItemActive}
+              />
             );
           })}
         </div>
@@ -42,5 +39,32 @@ export const Navigation = () => {
         </div>
       </div>
     </>
+  );
+};
+
+interface ISidebarEntity {
+  url: string;
+  name: string;
+  Icon: () => JSX.Element;
+  isItemActive: (url: string) => string;
+}
+
+const SidebarEntity: React.FC<ISidebarEntity> = ({
+  url,
+  name,
+  Icon,
+  isItemActive,
+}) => {
+  return (
+    <div className={`single-entity ${isItemActive(url)}`}>
+      <div className="navbar-icon">
+        <Icon />
+      </div>
+      <div className="navbar-text">
+        <span>
+          <Link to={url}>{name}</Link>
+        </span>
+      </div>
+    </div>
   );
 };
