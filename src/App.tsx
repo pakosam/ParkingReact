cbb1e772f698321f3612790fb104351880fc49f4
pakosam <pako.s@yahoo.com>
2025-04-03 16:55:35 +0200

    SignIn page done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import { SideBar } from "./components/Sidebar/SideBar";
 import { ParkingView } from "./pages/Parking/ParkingView";
+/*import { BrowserRouter, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";*/
+import { EmployeeView } from "./pages/EmployeeView";
+import { Register } from "./pages/Register";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import { EmployeeView } from "./pages/EmployeeView";
-import { Register } from "./pages/Register";
+import { SignIn } from "./pages/SignIn";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/registration" replace />} />
         <Route path="/registration" element={<Register />} />
+        <Route path="/">
+          <Route path="/" element={<Navigate to="/signin" replace />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
